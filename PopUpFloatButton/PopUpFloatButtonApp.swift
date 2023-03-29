f3736f89b46fcc3b178a67a4a896f88269b38d46
Cafe <dachscafe@yahoo.co.jp>
2023-03-29 20:14:05 +0900

    Initial Commit

---
+++ PopUpFloatButton/PopUpFloatButtonApp.swift
@@ -0,0 +1,17 @@
+//
+//  PopUpFloatButtonApp.swift
+//  PopUpFloatButton
+//
+//  Created by Cafe on 2023/03/29.
+//
+
+import SwiftUI
+
+@main
+struct PopUpFloatButtonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
